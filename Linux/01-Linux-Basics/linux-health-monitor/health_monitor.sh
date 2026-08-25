5e0f82e5a11835cd521a081ebe6b3bd00fd67afc
ankita4198 <ankita.katiyar.it@gmail.com>
2026-08-25 18:13:17 +0100

    Update health_monitor.sh

--- Linux/01-Linux-Basics/linux-health-monitor/health_monitor.sh
+++ Linux/01-Linux-Basics/linux-health-monitor/health_monitor.sh
@@ -2,7 +2,9 @@
 
 used_ram=$(free -h | grep "Mem" | awk '{print $3}')
 disk_usage=$(df -h | grep -w "/" | awk '{print $5}')
+cpu_usage=$(top -bn1 | grep "Cpu(s)")
 
-echo "Used RAM: $used_ram" >> health_log.txt
-echo "Disk Usage: $disk_usage" >> health_log.txt
-echo "------------------------" >> health_log.txt
+echo "Used RAM: $used_ram" >> /root/health_log.txt
+echo "Disk Usage: $disk_usage" >> /root/health_log.txt
+echo "CPU Usage: $cpu_usage" >> /root/health_log.txt
+echo "------------------------" >> /root/health_log.txt
